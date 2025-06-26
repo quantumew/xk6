fdf2b8549fcf117a66cd553970ec7e44d7d7d6ec
Andrew Gertjejansen <andrew.gertjejansen@mpls.frb.org>
2025-06-26 08:11:43 -0500

    Fix

--- cmd/xk6/main.go
+++ cmd/xk6/main.go
@@ -1,7 +1,7 @@
 // Package main contains the main entry point for the xk6 CLI.
 // This entry point exists for compatibility reasons,
-// so that you can still run xk6 with `go run quantumew/xk6/cmd/xk6@latest`.
-// If you want to use `go run`, use `go run quantumew/xk6@latest` instead.
+// so that you can still run xk6 with `go run github.com/quantumew/xk6/cmd/xk6@latest`.
+// If you want to use `go run`, use `go run github.com/quantumew/xk6@latest` instead.
 package main
 
 import "github.com/quantumew/xk6/internal/cmd"
